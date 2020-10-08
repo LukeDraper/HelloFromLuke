01b8aa1f5f06fc24fb66cb743e45b9c78ab3a6c3
Luke Draper <72437845+LukeDraper@users.noreply.github.com>
2020-10-07 19:44:01 -0600

    Initial commit

---
+++ src/draper/Main.java
@@ -0,0 +1,10 @@
+package draper;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Luke Draper.");
+
+    }
+}
